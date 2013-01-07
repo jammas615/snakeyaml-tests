c9301097f8721e88d8c0740150535d8cca57b836
James <James@James-Laptop>
2013-01-07 15:31:32 +1100

    Initial commit

--- snakeyaml-tests/src/net/james/snakeyaml/Test.java
+++ snakeyaml-tests/src/net/james/snakeyaml/Test.java
@@ -1,5 +1,27 @@
 package net.james.snakeyaml;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
 public class Test {
 
+	public static void main(String [] args) {
+		Yaml yaml = new Yaml();
+		
+		try {
+			InputStream input = new FileInputStream(new File("modpacks.yml"));
+			HashMap<String, String[]> data = (HashMap<String, String[]>) yaml.load(input);
+			String[] modpacks = data.get("modpacks");
+			System.out.println(data.get("modpacks"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
